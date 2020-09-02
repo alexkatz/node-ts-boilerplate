9900c1b6d50e1ec9e409bd6236f3311b0896a3cd
Alex Katz <alexanderikatz@gmail.com>
2020-09-01 21:16:28 -0400

    more boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import { luna, tycho } from './cats';
 
+const thing = 5;
+
+const fn = (foo: number, bar: string) => {
+  console.log(bar);
+};
+
 console.log(luna);
 console.log(luna.greeting());
 console.log(tycho);
